8f68ddef2ddc1b302463a62261e16bb5b6c38f7b
Jamezfn <jamesmakachia19gmail.com>
2024-05-16 08:32:53 -0700

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for num in range(9):
+    for k in range(num + 1, 10):
+        if num * 10 + k < 89:
+            print("{:d}{:d}".format(num, k), end=", ")
+print("{:d}".format(89))
