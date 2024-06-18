7bdd6c2b4f2ea7367f436223a7b58fb872577e36
James Mukhola <jamesmakachia19@gmail.com>
2024-06-19 00:07:14 +0300

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -12,26 +12,11 @@ class Rectangle {
 	}
 
 	print(){
-		if (this.width && this.height) {
-			let output = "";
-			for (let i = 0; i < this.width; i++) {
-				for (let j = 0; i < this.width; j++) {
-					output += "X";
-				}
-				output += "\n";
-			}
-			console.log(output);
-		} else {
-			console.log("This is not a rectangle");
-		}
+		for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
 	}
 
 	rotate() {
-		if (this.width && this.height) {
-			const temp = this.width;
-			this.width = this.height;
-			this.height = temp;
-		}
+		[this.width, this.height] = [this.height, this.width];
 	}
 
 	double() {
