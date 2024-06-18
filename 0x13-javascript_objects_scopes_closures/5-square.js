54ffb361cd19a16a3e64f27259b7f62b2d5f9312
James Mukhola <jamesmakachia19@gmail.com>
2024-06-19 00:25:15 +0300

    javas

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,50 +1,11 @@
 #!/usr/bin/node
 
-class Rectangle {
-	constructor(w, h){
-		if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
-			this.width = w;
-			this.height = h;
-		} else {
-			this.width = undefined;
-			this.height = undefined;
-		}
-	}
+const rectangle = require('./4-rectangle');
 
-	print(){
-		if (this.width && this.height) {
-			let output = "";
-			for (let i = 0; i < this.width; i++) {
-				for (let j = 0; i < this.width; j++) {
-					output += "X";
-				}
-				output += "\n";
-			}
-			console.log(output);
-		} else {
-			console.log("This is not a rectangle");
-		}
+class Square extends Rectangle {
+	constructor(size){
+		super(size, size);
 	}
-
-	rotate() {
-		if (this.width && this.height) {
-			const temp = this.width;
-			this.width = this.height;
-			this.height = temp;
-		}
-	}
-
-	double() {
-		if (this.width && this.height){
-			this.width *= 2;
-			this.height *= 2;
-		}
-	}
-}
-
-class Square extends Rectangle{
-	constructor(size);
-	super(size, size);
 }
 
 module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.esrever = function (list){
+module.exports.esrever = function (list){
 	const reversedList = [];
 	for (let i = list.length - 1; i >= 0; i--){
 		reversedList.push(list[i]);
