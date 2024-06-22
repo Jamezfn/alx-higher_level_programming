1e74b305c2f1c8d315e0c16621c6edf468b3c426
James Mukhola <jamesmakachia19@gmail.com>
2024-06-22 21:31:45 +0300

    Cities by states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,9 +11,9 @@ import MySQLdb
 
 
 def main():
-    conn = MySQLdb.connect:(
+    conn = MySQLdb.connect(
                         host="localhost",
-                        port=3306;
+                        port=3306,
                         user=sys.argv[1],
                         passwd=sys.argv[2],
                         db=sys.argv[3],
@@ -21,7 +21,7 @@ def main():
                             )
     cur = conn.cursor()
     search = sys.argv[4]
-    cur.execute("""SELECT id, name FROM states where name = %s
+    cur.execute("""SELECT id,name FROM states where name = %s
                 ORDER by id ASC""", (search,))
     row = cur.fetchall()
     for r in row:
