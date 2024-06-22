b039479aacd0fb6604df5b918013fb12a5d3da38
James Mukhola <jamesmakachia19@gmail.com>
2024-06-22 20:24:34 +0300

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument."""
-
+Write a script that takes in an argument and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
 import sys
 import MySQLdb
 
+
 def main():
     conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            charset="utf8"
-            )
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
     cur = conn.cursor()
-    state_name = sys.argv[4]
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query,(state_name,))
+    search = sys.argv[4]
+    query = """SELECT * FROM states where name = '{:s}'
+            ORDER by id ASC""".format(search)
+    cur.execute(query)
     row = cur.fetchall()
-
-    if row:
-        for r in row:
-            if r[1][0] == state_name
+    for r in row:
+        if r[1] == search:
             print(r)
-    else:
-        print("No state found with the name '{}'".fomat(state_name))
-
     cur.close()
     conn.close()
 
