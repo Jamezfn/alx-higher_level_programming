a6b0e3bd5ebb57ef932b085803bb2f73f770c8db
James Mukhola <jamesmakachia19@gmail.com>
2024-07-04 17:05:08 +0300

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a request, and displays the size of the response body in bytes
-curl -s -o /dev/null -w '%{size_download}\n' "$1"
+#script to trake url and display its size
+echo $(curl -s -w "%{size_download}\n" -o /dev/null "$1")
