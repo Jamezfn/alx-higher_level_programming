1a7af8e9e229898459a5025f5e49ff477347d4a9
Jamezfn <jamesmakachia19@gmail.com>
2025-06-18 09:57:12 +0300

    Cities by States

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVIDGES;
---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Creates a state table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+	(id INT UNIQUE AUTO_INCREMENT NOT NULL, name VARCHAR(256), PRIMARY KEY (id))
